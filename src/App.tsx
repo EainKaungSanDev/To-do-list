d38676504e74b0960db5efb8bdfb75e98ed25786
Eain Kaung San <unnoadd2325@gmail.com>
2026-07-23 16:26:26 +0000

    add filter search sort

--- src/App.tsx
+++ src/App.tsx
@@ -35,6 +35,33 @@ const [editId,setEditId] = useState< number | null >(null)
 
 const [editText,setEditText] = useState<string>("")
 
+type FilterStatus = "all" | "active" | "completed"
+    
+const [filter,setFilter] = useState<FilterStatus>("all")
+
+const [search,setSearch] = useState<string>("")
+
+type SortType = "newest" | "oldest";
+
+const [sort,setSort] = useState<SortType>("newest");
+
+const filteredList = array.filter(item => {
+
+const matchSearch = item.text.toLowerCase().includes(search.toLowerCase());
+    if(filter === "active")
+    return !item.completed && matchSearch;
+    if(filter === "completed")
+    return item.completed && matchSearch;
+    return matchSearch;
+})
+
+const sorted = [...filteredList].sort((a,b)=>{
+    if(sort === "newest")
+    return b.id - a.id
+    if(sort === "oldest")
+    return a.id - b.id
+})
+   
 const Change = (e:ChangeEvent<HTMLInputElement>): void => {
     setInput(e.target.value)
 }
@@ -51,35 +78,54 @@ const KeyDown = (e:KeyboardEvent<HTMLInputElement>): void => {
     } 
 }
 
-const Toggle = (id:number) => {
+const Toggle = (id:number): void => {
     setArray(prev => prev.map(item => item.id === id? {...item, completed : !item.completed} : item))
 } 
 
-const Edit = (id:number,text:string) => {
+const Edit = (id:number,text:string): void => {
     setEditId(id);
     setEditText(text)
 }
 
-const Save = () => {
-if(editId === null) return;
+const Save = (): void => {
+if(editId === null || editText.trim() === "") return;
     setArray(prev => prev.map(item => item.id === editId? {...item, text : editText} : item));
     setEditId(null);
     setEditText("");
 }
 
-const Delete = (id:number) => {
+const Delete = (id:number): void => {
     setArray(prev => prev.filter(item => item.id !== id))
 }
 
     return(
         <>
         <h1>To Do List</h1>
+         <input type="text" placeholder="search" value={search} onChange={(e)=> setSearch(e.target.value)} />
+         <hr/>
         <input type="text" placeholder="To do List" value={input} onChange={Change} onKeyDown={KeyDown} />
+        <hr/>
+        <div>
+          <button onClick={()=>setFilter("all")}>
+            All
+        </button> 
+        <button onClick={()=>setFilter("completed")}>
+           Completed 
+        </button> 
+        <button onClick={()=>setFilter("active")}>
+            Active
+        </button>
+        </div>        
+        
+        <div>
+            <button onClick={()=>setSort("newest")}>New</button>
+            <button onClick={()=>setSort("oldest")}>Old</button>
+        </div>
         
         <ul style={{
             listStyle : "none"
         }}>
-            {array.map(item => (
+            {sorted.map(item => (
                 <Child key={item.id} 
 item={item}
 onToggle={Toggle}
@@ -101,7 +147,7 @@ const Child = ({item,onToggle,onEdit,onSave,onDelete,editId,editText,setEditText
         <div>
           {
          editId === item.id?
-          <input value={editText} onChange = { (e:ChangeEvent<HTMLInputElement>) => {
+          <input autoFocus value={editText} onChange = { (e:ChangeEvent<HTMLInputElement>) => {
       setEditText(e.target.value)        
           }}/> :
           <span onClick={()=>onToggle(item.id)} style = {{
